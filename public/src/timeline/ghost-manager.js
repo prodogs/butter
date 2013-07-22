601ee964d9d909a927f263dfba35ae13f2887569
prodogs <prodogs@yahoo.com>
2013-07-22 11:52:34 -0400

    beautify

--- public/src/timeline/ghost-manager.js
+++ public/src/timeline/ghost-manager.js
@@ -1,94 +1,126 @@
 /* This Source Code Form is subject to the terms of the MIT license
  * If a copy of the MIT license was not distributed with this file, you can
  * obtain one at https://raw.github.com/mozilla/butter/master/LICENSE */
+define(["./ghost-track"], function (GhostTrack)
+{
 
-define( [ "./ghost-track" ], function( GhostTrack ) {
 
-  function GhostManager( media, tracksContainerElement ) {
 
-    var _media = media,
-        _tracksContainerElement = tracksContainerElement;
 
-    function createGhostTrackForTrack( track, nextTrack ) {
-      var ghostTrack;
-      if ( !track.ghost ) {
-        ghostTrack = track.ghost = new GhostTrack( track, nextTrack );
-        if ( !nextTrack ) {
-          _tracksContainerElement.appendChild( ghostTrack.view.element );
-        }
-        else {
-          _tracksContainerElement.insertBefore( ghostTrack.view.element, nextTrack.view.element );
-        }
-      }
-      return track.ghost;
-    }
+    function GhostManager(media, tracksContainerElement)
+    {
 
-    function cleanUpGhostTracks() {
-      var tracks = _media.tracks;
-      for ( var i = 0, l = tracks.length; i < l; ++i ) {
-        cleanUpGhostTrack( tracks[ i ] );
-      }
-    }
+        var _media = media,
+            _tracksContainerElement = tracksContainerElement;
 
-    function cleanUpGhostTrack( track ) {
-      var ghostTrack = track.ghost;
-      if ( ghostTrack && ghostTrack.numGhostTrackEvents === 0 ) {
-        _tracksContainerElement.removeChild( ghostTrack.view.element );
-        track.ghost = null;
-      }
-    }
+        function createGhostTrackForTrack(track, nextTrack)
+        {
 
-    this.trackEventDragged = function( trackEventView, trackView ) {
-      var track, nextTrack, ghostTrack,
-          overlappingTrackEvent;
+            var ghostTrack;
 
-      if ( trackView ) {
-        track = trackView.track;
-
-        overlappingTrackEvent = trackView.findOverlappingTrackEvent( trackEventView );
-
-        if ( overlappingTrackEvent ) {
-          nextTrack = _media.getNextTrack( track );
-          if ( !nextTrack || nextTrack.view.findOverlappingTrackEvent( trackEventView ) ) {
-            nextTrack = createGhostTrackForTrack( track, nextTrack );
-            if ( trackEventView.ghost && trackEventView.ghost.track !== nextTrack ) {
-              ghostTrack = trackEventView.ghost.track;
-              trackEventView.cleanupGhost();
-              if ( ghostTrack.lastTrack ) {
-                cleanUpGhostTrack( ghostTrack.lastTrack );
-              }
+            if (!track.ghost)
+            {
+                ghostTrack = track.ghost = new GhostTrack(track, nextTrack);
+                if (!nextTrack)
+                {
+                    _tracksContainerElement.appendChild(ghostTrack.view.element);
+                }
+                else
+                {
+                    _tracksContainerElement.insertBefore(ghostTrack.view.element, nextTrack.view.element);
+                }
             }
-          }
-          if ( !trackEventView.ghost ) {
-            nextTrack.view.addTrackEventGhost( trackEventView.createGhost() );
-          }
-          trackEventView.updateGhost();
+            return track.ghost;
         }
-        else if ( trackEventView.ghost ) {
-          track = trackEventView.ghost.track;
-          trackEventView.cleanupGhost();
-          cleanUpGhostTracks();
+
+
+        function cleanUpGhostTracks()
+        {
+            var tracks = _media.tracks;
+            for (var i = 0, l = tracks.length; i < l; ++i)
+            {
+                cleanUpGhostTrack(tracks[i]);
+            }
         }
-      }
-      else if ( trackEventView.ghost ) {
-        track = trackEventView.ghost.track;
-        trackEventView.cleanupGhost();
-        cleanUpGhostTracks();
-      }
-    };
 
-    this.removeGhostsAfterDrop = function( trackEvent ) {
-      var currentTrack = trackEvent.track,
-          ghost = trackEvent.view.ghost;
+        function cleanUpGhostTrack(track)
+        {
 
-      if ( ghost && ghost.track ) {
-        trackEvent.view.cleanupGhost( currentTrack );
-        cleanUpGhostTracks();
-      }
-    };
+            var ghostTrack = track.ghost;
 
-  }
+            if (ghostTrack && ghostTrack.numGhostTrackEvents === 0)
+            {
+                _tracksContainerElement.removeChild(ghostTrack.view.element);
+                track.ghost = null;
 
-  return GhostManager;
+            }
+        }
+
+        this.trackEventDragged =
+
+        function (trackEventView, trackView)
+        {
+
+            var track, nextTrack, ghostTrack,
+                overlappingTrackEvent;
+
+            if (trackView)
+            {
+                track = trackView.track;
+
+                overlappingTrackEvent = trackView.findOverlappingTrackEvent(trackEventView);
+
+                if (overlappingTrackEvent)
+                {
+                    nextTrack = _media.getNextTrack(track);
+                    if (!nextTrack || nextTrack.view.findOverlappingTrackEvent(trackEventView))
+                    {
+                        nextTrack = createGhostTrackForTrack(track, nextTrack);
+                        if (trackEventView.ghost && trackEventView.ghost.track !== nextTrack)
+                        {
+                            ghostTrack = trackEventView.ghost.track;
+                            trackEventView.cleanupGhost();
+                            if (ghostTrack.lastTrack)
+                            {
+                                cleanUpGhostTrack(ghostTrack.lastTrack);
+                            }
+                        }
+                    }
+                    if (!trackEventView.ghost)
+                    {
+                        nextTrack.view.addTrackEventGhost(trackEventView.createGhost());
+                    }
+                    trackEventView.updateGhost();
+                }
+                else if (trackEventView.ghost)
+                {
+                    track = trackEventView.ghost.track;
+                    trackEventView.cleanupGhost();
+                    cleanUpGhostTracks();
+                }
+            }
+            else if (trackEventView.ghost)
+            {
+                track = trackEventView.ghost.track;
+                trackEventView.cleanupGhost();
+                cleanUpGhostTracks();
+            }
+        };
+
+        this.removeGhostsAfterDrop = function (trackEvent)
+        {
+            var currentTrack = trackEvent.track,
+                ghost = trackEvent.view.ghost;
+
+            if (ghost && ghost.track)
+            {
+                trackEvent.view.cleanupGhost(currentTrack);
+                cleanUpGhostTracks();
+            }
+        };
+
+    }
+
+    return GhostManager;
 
 });
